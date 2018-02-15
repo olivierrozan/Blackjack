7cdc875a2ba19af70c61ca171665923d788a7e7b
olivier <rozan.olivier@gmail.com>
2018-02-15 23:10:06 +0100

    mise inférieure ou égale à l'argent total

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -69,6 +69,9 @@ export class AppComponent implements OnInit {
 
     bet(bet: number): void {
         this.playerBet += bet;
+        if (this.playerBet > this.money) {
+            this.playerBet = this.money;
+        }
     }
 
     startGame(): void {
